470d3eeafe90b6e246170bcdc9708907a8ca2c08
bellezzasky <54636880+Dityath@users.noreply.github.com>
2021-07-01 23:03:51 +0700

    Update copy-this.js

--- copy-this.js
+++ copy-this.js
@@ -1,7 +1,7 @@
 //copy code below to console
 let r = document.querySelectorAll('input[type=radio]');
-for (var i = 0; i < r.length; i += 1){
-    if(i % 1 == 0){
+for (var i = 0; i < r.length; i += 2){
+    if(i % 2 == 0){
         r[i].checked = true;}
 }
 document.forms["form1"].submit();
