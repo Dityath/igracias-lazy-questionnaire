8a84ce24f74d982eec1cf606c13e098cecfb0009
Ditya Athallah <54636880+Dityath@users.noreply.github.com>
2021-10-14 07:54:56 +0700

    Update copy-this.js

--- copy-this.js
+++ copy-this.js
@@ -1,6 +1,6 @@
 //copy code below to console
 let r = document.querySelectorAll('input[type=radio]');
-for (var i = 0; i < r.length; i += 2){
+for (var i = 0; i < r.length; i += 4){
     if(i % 2 == 0){
         r[i].checked = true;}
 }
