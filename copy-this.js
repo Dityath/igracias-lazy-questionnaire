77e9f96af11cc269c844599fefb37a982a9a11e7
Bagas Tri Wibowo <bagas.tri@tanihub.com>
2022-06-25 00:43:42 +0700

    [MODIFY] optimze the result

--- copy-this.js
+++ copy-this.js
@@ -1,7 +1,11 @@
 //copy code below to console
 let r = document.querySelectorAll('input[type=radio]');
-for (var i = 0; i < r.length; i += 4){
+let numberOfRadionButton = 5
+for (var i = 0; i < r.length; i += numberOfRadionButton){
     if(i % 2 == 0){
-        r[i].checked = true;}
+        r[i].checked = true;
+    }else{
+        r[i+1].checked = true; // erase the + 1 if you want to make all "sangat puas"
+    }
 }
 document.forms["form1"].submit();
